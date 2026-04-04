51b6e113e3936213694cfb9f4dfe2c537bff7691
nokteholda <reyhanmad@gmail.com>
2026-04-04 22:16:12 +0700

    test diff map

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const proxyURL = 'https://api.codetabs.com/v1/proxy/?quest='
-const mapURL = 'https://map.earthmc.net/tiles'
+const mapURL = 'https://nostra.earthmc.net/tiles'
 const highwaysURL = 'https://raw.githubusercontent.com/XiLeF2211/ice-highways-map/refs/heads/main/highways.json'
 const netherHighwaysURL = 'https://raw.githubusercontent.com/XiLeF2211/ice-highways-map/refs/heads/main/netherHighways.json'
 
