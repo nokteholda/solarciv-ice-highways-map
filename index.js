a44c6099f8ec6ae99545944078c0c61fd409aa00
Stanley Bauer <stanleygab37@doinyour.mom>
2026-04-21 16:52:41 -0400

    Changed index.json to pull Highways.json from correct repo.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const projection = 128;
 
 const proxyURL = 'https://api.codetabs.com/v1/proxy/?quest='
 const mapURL = 'https://map.solarciv.com/tiles'
-const highwaysURL = 'https://raw.githubusercontent.com/XiLeF2211/ice-highways-map/refs/heads/main/highways.json'
+const highwaysURL = 'https://raw.githubusercontent.com/nokteholda/solarciv-ice-highways-map/refs/heads/main/highways.json'
 const netherHighwaysURL = 'https://raw.githubusercontent.com/XiLeF2211/ice-highways-map/refs/heads/main/netherHighways.json'
 
 let highwayData;
